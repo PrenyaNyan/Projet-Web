4decfb94d1d71b842b5e155d4f60696ba9c499d9
Alexandre <alexandrepereon8@gmail.com>
2022-03-16 09:48:38 +0100

    let's go

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <title>DepiStage</title>
-    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="./assets/css/index.css">
 </head>
