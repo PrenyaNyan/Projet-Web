35bb691068ff7d56702622a1c1d7b811952ef5d0
Alexandre <alexandrepereon8@gmail.com>
2022-03-17 11:04:18 +0100

    let's go

--- assets/html/login.php
+++ assets/html/login.php
@@ -13,29 +13,29 @@ if (isset($_SESSION["newsession"])) {
     <meta charset="utf-8">
     <title>DepiStage</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="./assets/css/index.css">
+    <link rel="stylesheet" href="../vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../css/login-signup.css">
 </head>
 
 <body class="text-center">
     <main class="form-signin">
-        <form action="index.php" method="post">
+        <form action="login.php" method="get">
 
             <img src="https://cdn.discordapp.com/attachments/950033739604434965/950403057567551528/logo.png" width="120">
-            <h1 class="h3 mb-3 fw-normal">Sign in DepiStage</h1>
+            <h1 class="h3 mb-3 fw-normal">Login DepiStage</h1>
             <div id="FormContent">
                 <div class="form-floating">
                     <input class="form-control FirstInput" id="floatingInput" placeholder="Username" name="Username">
                     <label for="floatingInput">Username</label>
                 </div>
                 <div class="form-floating">
-                    <input class="form-control LastInput" id="floatingPassword" placeholder="Password" name="Password">
+                    <input type="password" class="form-control LastInput" id="floatingPassword" placeholder="Password" name="Password">
                     <label for="floatingPassword">Password</label>
                 </div>
             </div>
             <div id="FormButton">
-                <button type="button" class="btn btn-lg btn-primary" id="SignUpForm">Sign Up</button>
+                <button type="button" class="btn btn-lg btn-primary" id="SignUpForm" onclick="window.location.href='signup.php';">Sign Up</button>
                 <button type="submit" class="btn btn-secondary btn-lg" id="SignInSubmit">Submit</button>
             </div>
 
@@ -46,13 +46,13 @@ if (isset($_SESSION["newsession"])) {
             $dbuser = "pipou";
             $dbpasswd = "azertyuiop";
 
-            if (isset($_POST["Username"]) && isset($_POST["Password"])) {
+            if (isset($_GET["Username"]) && isset($_GET["Password"])) {
                 try {
                     $pdo = new PDO('mysql:host=' . $dbhost . ';port=' . $dbport . ';dbname=' . $db . '', $dbuser, $dbpasswd);
 
                     $stmt = $pdo->prepare("SELECT * FROM `users` where USERNAME=? and PASSWORD=?");
-                    $stmt->bindParam(1, $_POST["Username"]);
-                    $stmt->bindParam(2, $_POST["Password"]);
+                    $stmt->bindParam(1, $_GET["Username"]);
+                    $stmt->bindParam(2, $_GET["Password"]);
                     $stmt->execute();
                     $res = $stmt->fetch();
 
@@ -69,7 +69,7 @@ if (isset($_SESSION["newsession"])) {
                 $stmt->closeCursor();
 
                 if (isset($_SESSION["newsession"])) {
-                    header("Location: /assets/html/accueil.php");
+                    header("Location: accueil.php");
                     exit();
                 }
             }
@@ -80,8 +80,8 @@ if (isset($_SESSION["newsession"])) {
 
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-    <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="./assets/js/scriptIndex.js"></script>
+    <script src="../vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+    
 </body>
 
 </html>
---
+++ assets/html/signup.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+if (isset($_SESSION["newsession"])) {
+    header("Location: /assets/html/accueil.php");
+    exit();
+}
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>DepiStage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../css/login-signup.css">
+</head>
+
+<body class="text-center">
+    <main class="form-signin">
+        <form action="signup.php" method="get">
+
+            <img src="https://cdn.discordapp.com/attachments/950033739604434965/950403057567551528/logo.png" width="120">
+            <h1 class="h3 mb-3 fw-normal">Sign Up DepiStage</h1>
+            <div id="FormContent">
+                <div class="form-floating">
+                    <input class="form-control FirstInput" id="floatingName" placeholder="Name" name="name">
+                    <label for="floatingName">Name</label>
+                </div>
+                <div class="form-floating">
+                    <input class="form-control MiddleInput" id="floatingFirstName" placeholder="First Name" name="firstname">
+                    <label for="floatingFirstName">First Name</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control MiddleInput" id="SignUpPassord" placeholder="Password" name="password">
+                    <label for="SignUpPassord">Password</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control MiddleInput" id="PasswordConfirmation" placeholder="PasswordConfirmation" name="passwordconfirm">
+                    <label for="PasswordConfirmation">Password Confirmation</label>
+                </div>
+                <div class="form-floating">
+                    <select class="form-select LastInput" aria-label="Default select example">
+                        <option selected>Select a role</option>
+                        <option value="1">Admin</option>
+                        <option value="2">Pilote</option>
+                        <option value="3">Etudiant</option>
+                        <option value="4">Delegue</option>
+                    </select>
+                </div>
+
+            </div>
+            <div id="FormButton">
+                <button type="button" class="btn btn-primary btn-lg" id="SignInForm" onclick="window.location.href='login.php';">Login</button>
+                <button class="btn btn-secondary btn-lg espace" type="submit" id="SignUpSubmit" method="post">Submit</button>
+            </div>
+
+            
+            <p class="mt-5 mb-3 text-muted">© 2021 DepiStage, Inc. All rights reserved.</p>
+        </form>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <script src="../vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
