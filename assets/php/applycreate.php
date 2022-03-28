4c5f4f23a72630c935f68abf81040bb0f760e821
Malo KERAUTRET <malo.kerautret@viacesi.fr>
2022-03-28 11:35:19 +0200

    11h35 28/03

--- assets/php/applycreate.php
+++ assets/php/applycreate.php
@@ -1,14 +1,17 @@
 <?php
 
 try {
-    $stmt = $pdo->prepare(" SELECT ID_USER 
+    $stmt = $pdo->prepare(" SELECT ID_Session 
     FROM users 
     WHERE users.USERNAME = ?;");
     $stmt->bindParam(1, $_SESSION["newsession"]);
     $stmt->execute();
     $res = $stmt->fetchAll();
     $stmt->closeCursor();
-    echo($_SESSION["newsession"]);
+
+
+    print_r($res);
+    
 } catch (\Throwable $th) {
     echo '<div class="alert alert-danger" role="alert">
     This is a danger alert—check it out!</div>';
